c8885daff822f2808a032a7f4d0b89a346be58b7
roscxn <roscxn@gmail.com>
2023-04-17 16:25:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,10 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
-});
-
-app.use(express.static(path.join(__dirname, 'build')));
-
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
 });
